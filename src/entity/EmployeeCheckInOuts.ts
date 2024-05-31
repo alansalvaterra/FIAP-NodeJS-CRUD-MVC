979f8297bb287fcab92c7481367f04074d3d26aa
alansalvaterra <alan.salvaterra@gmail.com>
2024-05-31 17:00:10 -0300

    refactor: Adjust SQL table columns

--- src/entity/EmployeeCheckInOuts.ts
+++ src/entity/EmployeeCheckInOuts.ts
@@ -8,29 +8,14 @@ export class EmployeeCheckInOuts {
     id: number
 
     @Column()
-    date: Date
-
-    @Column()
     checkIn: Date
 
     @Column()
     checkOut: Date
 
-    @Column({ type: 'float', nullable: true })
-    workedTime: number
-
     @ManyToOne(() => Users, (users) => users.employeeCheckInOuts, { eager: true })
     @JoinColumn({ name: 'userId' })
     users: Users;
     static users: any
-
-    @BeforeInsert()
-    @BeforeUpdate()
-    calculateWorkedTime() {
-        if (this.checkIn && this.checkOut) {
-            const diffInMillis = this.checkOut.getTime() - this.checkIn.getTime();
-            this.workedTime = diffInMillis / (1000 * 60 * 60); // converter milissegundos para horas
-        }
-    }
 }
 
