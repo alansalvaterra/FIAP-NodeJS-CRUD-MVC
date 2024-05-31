18d55d9a4420f212c7f412c3cb487acb87a62a7a
alansalvaterra <alan.salvaterra@gmail.com>
2024-05-30 22:40:53 -0300

    fix: correcting routes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as express from "express"
 import * as bodyParser from "body-parser"
 import { Request, Response } from "express"
 import { AppDataSource } from "./data-source"
-import { Routes } from "./routes"
+import { Routes } from "./routes/userRoutes"
 
 AppDataSource.initialize().then(async () => {
 
